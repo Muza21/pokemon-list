1fda6a9373abe758505fc335716dd58e32776021
Muza21 <lasha.muzashvili1@gmail.com>
2025-04-16 23:32:16 +0400

    style: add header component, remove vite logo, and add vercel link to readme

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <>
+        <header>
+            <h1>pokemon-list</h1>
+            <button>favorites</button>
+            <button>comparison</button>
+        </header>
+        </>
+    )
+}
+
+export default Header;
