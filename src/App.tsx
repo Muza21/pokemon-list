3aba5791907f668d341c886736acb38aacbd4dcd
Muza21 <lasha.muzashvili1@gmail.com>
2025-04-24 19:39:27 +0400

    feat: add compare pokemon page and import remove icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import FavouritePokemonList from "./components/FavouritePokemonList/FavouritePokemonList";
+import ComparePokemon from "./components/ComparePokemon/ComparePokemon";
+// import FavouritePokemonList from "./components/FavouritePokemonList/FavouritePokemonList";
 import Header from "./components/Header/Header";
 // import PokemonInfo from "./components/PokemonInfo/PokemonInfo";
 // import PokemonList from "./components/PokemonList/PokemonList";
@@ -10,7 +11,8 @@ function App() {
       <Header />
       {/* <PokemonList /> */}
       {/* <PokemonInfo /> */}
-      <FavouritePokemonList />
+      {/* <FavouritePokemonList /> */}
+      <ComparePokemon />
     </>
   );
 }
