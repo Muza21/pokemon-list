4d4d22855012c6c6305a0357d892fe62ab09e912
Muza21 <lasha.muzashvili1@gmail.com>
2025-04-27 23:34:56 +0400

    fix: navigation to specific pokemon detail page, so button click will not trigger it

--- src/components/PokemonItem/PokemonItem.tsx
+++ src/components/PokemonItem/PokemonItem.tsx
@@ -4,7 +4,7 @@ import Plus from "../../assets/Icons/Plus";
 import Star from "../../assets/Icons/Star";
 import Button from "../Button/Button";
 import styles from "./PokemonItem.module.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export type Pokemon = {
   name: string;
@@ -14,35 +14,35 @@ export type Pokemon = {
 };
 
 const PokemonItem = ({ name, id, isFavorite, isInComparison }: Pokemon) => {
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    console.log(name);
+    navigate(`/details/${id}`);
   };
   return (
     <>
-      <Link to={`/details/${id}`}>
-        <div className={styles.pokemon_card} onClick={handleClick}>
-          <div className={styles.header}>
-            <h3 className={styles.name}>{name}</h3>
-            <p className={styles.id}>{id}</p>
-          </div>
-          <div className={styles.buttons_container}>
-            <Button
-              onClick={(e: MouseEvent<HTMLButtonElement>) => {
-                e.stopPropagation();
-              }}
-            >
-              <Star filled={isFavorite} />
-            </Button>
-            <Button
-              onClick={(e: MouseEvent<HTMLButtonElement>) => {
-                e.stopPropagation();
-              }}
-            >
-              {isInComparison ? <Minus /> : <Plus />}
-            </Button>
-          </div>
+      <div className={styles.pokemon_card} onClick={handleClick}>
+        <div className={styles.header}>
+          <h3 className={styles.name}>{name}</h3>
+          <p className={styles.id}>{id}</p>
         </div>
-      </Link>
+        <div className={styles.buttons_container}>
+          <Button
+            onClick={(e: MouseEvent<HTMLButtonElement>) => {
+              e.stopPropagation();
+            }}
+          >
+            <Star filled={isFavorite} />
+          </Button>
+          <Button
+            onClick={(e: MouseEvent<HTMLButtonElement>) => {
+              e.stopPropagation();
+            }}
+          >
+            {isInComparison ? <Minus /> : <Plus />}
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
