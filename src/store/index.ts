527b7bd6a8c44008709cb385b5357ce90b67438c
Muza21 <lasha.muzashvili1@gmail.com>
2025-05-22 12:20:13 +0400

    feat: install redux-persist, add persist feature for favorite pokemons, and add persist gate

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import pokemonsReducer from "./pokemons/slice";
 import favoritesReducer from "./favourites/slice";
 import { useDispatch } from "react-redux";
 import comparisonReducer from "./comparison/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const rootReducer = {
+const rootPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["favorites"],
+};
+
+const rootReducer = combineReducers({
   pokemons: pokemonsReducer,
   favorites: favoritesReducer,
   comparison: comparisonReducer,
-};
-
-export const store = configureStore({
-  reducer: rootReducer,
 });
 
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = typeof store.dispatch;
