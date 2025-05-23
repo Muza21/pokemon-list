3a31e7f034de8274db97a045a3b13855020e943b
Muza21 <lasha.muzashvili1@gmail.com>
2025-05-23 18:56:37 +0400

    test: format and pokemon util functions

--- src/tests/format.test.ts
+++ src/tests/format.test.ts
@@ -2,7 +2,12 @@ import { expect, test } from "vitest";
 
 import { formatHeight, formatWeight } from "../utils/format";
 
-test("should work as expected", () => {
-  expect(formatHeight(70)).toBe("7 m");
-  expect(formatWeight(50)).toBe("5 kg");
+test("The format height function returns the given number divided by 10 with unit", () => {
+  expect(formatHeight(77)).toBe("7.7 m");
+  expect(formatHeight(170)).toBe("17 m");
+});
+
+test("The format weight function returns the given number divided by 10 with unit", () => {
+  expect(formatWeight(55)).toBe("5.5 kg");
+  expect(formatWeight(152)).toBe("15.2 kg");
 });
